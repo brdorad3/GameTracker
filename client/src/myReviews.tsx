780a172f8b88794e1da9b703c2bd0f9c01db0c33
brdorad3 <vunicadis@gmail.com>
2024-08-09 21:01:11 +0200

    review list

--- client/src/myReviews.tsx
+++ client/src/myReviews.tsx
@@ -4,7 +4,8 @@ import { useLocation } from "react-router-dom"
 const MyReviews = () => {
 const navigate = useLocation()
   console.log(navigate.state)
-
+  const res = navigate.state
+    console.log(res)
     return(
         <>
         <Navbar></Navbar>
@@ -14,9 +15,37 @@ const navigate = useLocation()
             </div>
             <div className="w-full  h-full flex items-end justify-center">
         <div className="w-[90%] bg-acc h-[80%] flex justify-evenly">
-            <div className="grow-[2] flex justify-center">game</div>
-            <div className=" grow flex justify-center">status</div>
-            <div className=" grow flex justify-center">score</div>
+            <div className="grow-[2] flex flex-col">
+                <div className="flex justify-between px-8 py-7">
+                    <div className="w-[60%] flex justify-center ">Game</div>
+                    <div className="w-[20%]  flex justify-center">Status</div>
+                    <div className="w-[20%]  flex justify-center">Score</div>
+                </div>
+                
+                <div className="flex flex-col gap-5 ">
+                {
+                    res &&
+                    res.map((slot: any) => (
+                        <div className="flex justify-between px-8">
+                            <div className="flex grow-[5] max-w-[60%] gap-3">
+                        <img src={slot.cover} alt="" className="w-12 h-16" />
+                        <h1 className="pt-1">{slot.game}</h1>
+                        </div>
+                        <div className="w-[20%] flex justify-center">
+                            <p>{slot.status}</p>
+                        </div>
+                        <div className="w-[20%] flex justify-center">
+                            <p>{slot.rating}</p>
+                        </div>
+                        </div>
+                    ))
+                }
+                </div>
+                <div className="relative w-full h-full">
+               
+            </div>
+            </div>
+
         </div>
             </div>
 
